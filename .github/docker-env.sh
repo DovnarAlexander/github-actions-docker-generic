68a0f96f53929c8abf8e5ff3857f32dbc5272e12
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-09-16 12:56:29 +0300

    Revert "wip"
    This reverts commit f91e2980240bb4926fa633aed6b3bd347bc6ce66.

--- .github/docker-env.sh
+++ .github/docker-env.sh
@@ -2,12 +2,12 @@
 
 VERSION=noop
 
-if [ "$EVENT" = "schedule" ]; then
+if [ $EVENT = "schedule" ]; then
 VERSION=nightly
 elif [[ $GITHUB_REF == refs/tags/* ]]; then
 VERSION=${GITHUB_REF#refs/tags/}
 elif [[ $GITHUB_REF == refs/heads/* ]]; then
-VERSION=$(echo "${GITHUB_REF#refs/heads/}" | sed -r 's#/+#-#g')
+VERSION="$(echo ${GITHUB_REF#refs/heads/} | sed -r 's#/+#-#g')"
 if [ "$DEFAULT_BRANCH" = "$VERSION" ]; then
     VERSION=edge
 fi
@@ -19,7 +19,7 @@ if [[ $VERSION =~ ^v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 MINOR=${VERSION%.*}
 MAJOR=${MINOR%.*}
 TAGS="$TAGS,${DOCKER_IMAGE}:${MINOR},${DOCKER_IMAGE}:${MAJOR},${DOCKER_IMAGE}:latest"
-elif [ "$EVENT" = "push" ]; then
+elif [ "${{ github.event_name }}" = "push" ]; then
 TAGS="$TAGS,${DOCKER_IMAGE}:sha-${GITHUB_SHA::8}"
 fi
 echo ::set-output name=version::"${VERSION}"
