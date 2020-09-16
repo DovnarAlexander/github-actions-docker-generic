bff952cec441c9fda16bac555e451c3c66943310
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-09-16 12:56:30 +0300

    Revert "Lint fixed for bash"
    This reverts commit 41ab433c2e22803b89eebdee72585939d173a07e.

--- docker-env.sh
+++ docker-env.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+DOCKER_IMAGE=docker.pkg.github.com/dovnaralexander/github-actions-docker-generic/service
 VERSION=noop
 
-if [ $EVENT = "schedule" ]; then
+if [ "${{ github.event_name }}" = "schedule" ]; then
 VERSION=nightly
 elif [[ $GITHUB_REF == refs/tags/* ]]; then
 VERSION=${GITHUB_REF#refs/tags/}
 elif [[ $GITHUB_REF == refs/heads/* ]]; then
-VERSION="$(echo ${GITHUB_REF#refs/heads/} | sed -r 's#/+#-#g')"
-if [ "$DEFAULT_BRANCH" = "$VERSION" ]; then
+VERSION=$(echo ${GITHUB_REF#refs/heads/} | sed -r 's#/+#-#g')
+if [ "${{ github.event.repository.default_branch }}" = "$VERSION" ]; then
     VERSION=edge
 fi
 elif [[ $GITHUB_REF == refs/pull/* ]]; then
-VERSION=pr-$EVENT_NUMBER
+VERSION=pr-${{ github.event.number }}
 fi
 TAGS="${DOCKER_IMAGE}:${VERSION}"
 if [[ $VERSION =~ ^v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
@@ -22,6 +23,6 @@ TAGS="$TAGS,${DOCKER_IMAGE}:${MINOR},${DOCKER_IMAGE}:${MAJOR},${DOCKER_IMAGE}:la
 elif [ "${{ github.event_name }}" = "push" ]; then
 TAGS="$TAGS,${DOCKER_IMAGE}:sha-${GITHUB_SHA::8}"
 fi
-echo ::set-output name=version::"${VERSION}"
-echo ::set-output name=tags::"${TAGS}"
-echo ::set-output name=created::"$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+echo ::set-output name=version::${VERSION}
+echo ::set-output name=tags::${TAGS}
+echo ::set-output name=created::$(date -u +'%Y-%m-%dT%H:%M:%SZ')
