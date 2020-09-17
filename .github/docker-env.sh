6518207e29c5f35d2e49c670c7c09ebca04219fe
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-09-17 12:44:14 +0300

    Lint fixed

--- .github/docker-env.sh
+++ .github/docker-env.sh
@@ -13,7 +13,7 @@ then
     VERSION=$(echo "${GITHUB_REF#refs/heads/}" | sed -r 's#/+#-#g')
     if [ "master" == "$VERSION" ]
     then
-        VERSION="${TAG},latest"
+        VERSION="${NEW_TAG},latest"
     fi
 elif [[ $GITHUB_REF == refs/pull/* ]]
 then
